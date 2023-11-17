9d0e291a3f1a15bde044ac70ea9a965e1d13b2cb
Garry <garridio85@hotmail.co.uk>
2023-11-17 11:28:33 -0500

    wip

--- src/views/home/home.view.tsx
+++ src/views/home/home.view.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import Papa from 'papaparse';
 import Markdown from 'markdown-to-jsx';
+import nurseMain from '../../assets/nurse-main.png';
+import spirometer from '../../assets/spirometer.png';
 
 export const Home = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -45,55 +47,75 @@ export const Home = () => {
 
   return (
     <div style={{ maxWidth: 1220, margin: 'auto', padding: 8 }}>
-      <div style={{ textAlign: 'center' }}>
-        {homeData.map((data: any, index: number) => (
-          <>
-            {index === 0 && (
-              <div style={{ marginBottom: 40 }} key={data.section}>
-                {data.sectionTitle && (
-                  <h1>
-                    <Markdown>{data.sectionTitle}</Markdown>
-                  </h1>
-                )}
-                {data.content && (
-                  <div
-                    style={{
-                      marginBottom: 50,
-                      width: '80%',
-                      margin: 'auto',
-                      fontSize: '22px',
-                      color: '#1b1b1b',
-                    }}
-                  >
-                    <Markdown>{data.content || ''}</Markdown>
-                  </div>
-                )}
-              </div>
-            )}
-          </>
-        ))}
+      <div className="home-welcome-container">
+        <div>
+          <h1 style={{ fontSize: 45, width: '80%' }}>
+            Inspire Your Lungs: Your Guide to Incentive Spirometry Success
+          </h1>
+          <p style={{ color: '#434343', fontSize: '18px' }}>
+            Here at our website, our goal is to boost your understanding and
+            ease when it comes to using incentive spirometry. We are dedicated
+            to providing accessible information that helps you feel more
+            confident and comfortable in incorporating this beneficial practice
+            into your healthcare routine.
+          </p>
+        </div>
+        <div>
+          <img src={nurseMain} alt="nurses talking" style={{ height: 420 }} />
+        </div>
+      </div>
+      <div className="blue-bg-container">
+        <img
+          src={spirometer}
+          alt="nurses talking"
+          style={{
+            height: '390px',
+            border: 'solid 6px white',
+            borderRadius: '8px',
+          }}
+        />
 
+        <div className="spirometry-container" style={{ marginLeft: 60 }}>
+          <p style={{ fontSize: '20px', color: 'white', marginBottom: 60 }}>
+            Incentive spirometry is a respiratory therapy device used to help
+            improve lung function. It's often used after surgery or during
+            certain lung conditions. The device encourages the user to take
+            slow, deep breaths by inhaling through a mouthpiece, which lifts a
+            marker, providing visual feedback. This process helps keep the lungs
+            clear of mucus and increases lung capacity by encouraging full lung
+            inflation. It's particularly important for patients who are
+            recovering from surgery or have conditions that might lead to lung
+            complications, as it aids in preventing pneumonia and other
+            respiratory problems.
+          </p>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '8px',
+              width: '400px',
+            }}
+          >
+            <a href="/general">
+              <button className="blue-box-button">
+                Learn More About Incentive Spirometry
+              </button>
+            </a>
+            <a href="/how-to">
+              <button className="blue-box-button">
+                Learn How to Use an Incentive Spirometer
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div style={{ textAlign: 'center', marginTop: 50, marginBottom: 200 }}>
         <div style={{ display: 'flex', gap: 8, flexWrap: 'wrap' }}>
           <div className="blue-box">
             <h2>Test your Knowledge</h2>
             <button className="blue-box-button">
               <a href="/quiz">Click Here To Take Our Quiz</a>
-            </button>
-          </div>
-
-          <div className="blue-box">
-            <h2>But What is Incentive Spirometry?</h2>
-            <button className="blue-box-button">
-              <a href="/general">
-                Click Here To Learn More About Incentive Spirometry
-              </a>
-            </button>
-          </div>
-
-          <div className="blue-box">
-            <h2>How to Use an Incentive Spirometer</h2>
-            <button className="blue-box-button">
-              <a href="/how-to">Click Here For "How To" Video</a>
             </button>
           </div>
 
